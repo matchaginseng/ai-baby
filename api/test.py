72f85b65852de6a6290459e8e6d6e2814e5a3946
VICKI XU <vickixu20@gmail.com>
2026-02-02 21:06:09 -0500

    deploy

--- api/test.py
+++ api/test.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from http.server import BaseHTTPRequestHandler
+import json
 
-app = Flask(__name__)
-
-@app.route("/api/test")
-def test():
-    return {"status": "test works"}
-
-handler = app
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
+        self.wfile.write(json.dumps({"status": "python works"}).encode())
+        return
